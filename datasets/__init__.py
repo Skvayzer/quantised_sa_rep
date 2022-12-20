75394200eeb01e2d556174fc4bc360e5343a5242
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-20 11:03:40 +0300

    added tetrominoes, multi-dsprites

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,6 @@
 from .clevr import CLEVR
 from .clevrtex import  CLEVRTEX, CLEVRTEX_Evaluator, collate_fn
+from .multi_dsprites import MultiDSprites
+from .tetrominoes import Tetrominoes
 
-__all__ = ['CLEVR', 'CLEVRTEX', 'CLEVRTEX_Evaluator', 'collate_fn']
+__all__ = ['CLEVR', 'CLEVRTEX', 'CLEVRTEX_Evaluator', 'collate_fn', 'Tetrominoes', 'MultiDSprites']
