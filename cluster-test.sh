56e5ed4edaefa31b8e0e428eaf2b2bfeb1ef24c0
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-24 10:39:05 +0300

    added test run script for cluster

--- cluster-test.sh
+++ cluster-test.sh
@@ -24,6 +24,7 @@ singularity exec instance://ml_env /bin/bash -c "
       cd /home/quantised_sa/;
       pwd;
       ls;
+      echo "lala";
       python3 quantized_sa_rep/training_od.py --dataset 'clevr-tex' --device 'gpu' --max_epochs 442 --batch_size 512 --train_path "/home/quantized_sa/datasets/clevr-tex" --seed 0 --nums 8 8 8 8 --num_workers 2 ;
       free -m;
 " > output.txt
