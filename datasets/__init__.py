cff319760b623bb25f53ad61589f819106779e6c
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-25 21:19:38 +0300

    prepared clevr-tex test

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
 from .clevr import CLEVR
-from .clevrtex import  CLEVRTEX, CLEVRTEX_Evaluator, collate_fn
+from .clevrtex import  CLEVRTEX, collate_fn
 from .multi_dsprites import MultiDSprites
 from .tetrominoes import Tetrominoes
 
-__all__ = ['CLEVR', 'CLEVRTEX', 'CLEVRTEX_Evaluator', 'collate_fn', 'Tetrominoes', 'MultiDSprites']
+__all__ = ['CLEVR', 'CLEVRTEX', 'collate_fn', 'Tetrominoes', 'MultiDSprites']
