bb55588290a44d858adc62d927ef067ebbf2b1b2
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-29 15:39:44 +0300

    prepared clevr-tex test

--- datasets/clevrtex.py
+++ datasets/clevrtex.py
@@ -196,16 +196,17 @@ class CLEVRTEX:
 def collate_fn(batch):
     # return (
     #     *torch.utils.data._utils.collate.default_collate([(b[0], b[1], b[2]) for b in batch]), [b[3] for b in batch])
-    print('BATCH INFO ', batch[0], type(batch), file=sys.stderr, flush=True)
-    # images = torch.tensor([b['image'] for b in batch])
-    # masks = torch.tensor([b['mask'] for b in batch])
-    # targets = torch.tensor([b['target'] for b in batch])
-    # indexes = torch.tensor([b['index'] for b in batch])
+    # print('BATCH INFO ', batch, type(batch), file=sys.stderr, flush=True)
+    images = torch.stack([b['image'] for b in batch])
+    masks = torch.stack([b['mask'] for b in batch])
+    targets = torch.stack([b['target'] for b in batch])
+    indexes = torch.stack([b['index'] for b in batch])
+
     return {
-        'image': batch['image'],
-        'mask': batch['mask'],
-        'target': batch['target'],
-        'index': batch['index']
+        'image': images,
+        'mask': masks,
+        'target': targets,
+        'index': indexes
     }
 
 
