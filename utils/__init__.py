1f13a0e9983a4c6bfcca6cd6cba949250348d638
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-29 17:01:17 +0300

    prepared clevr-tex test

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .spatial_utils import spatial_flatten, spatial_broadcast
 from .grid import build_grid
 from .loss_func import hungarian_huber_loss
-from .metrics import Evaluator
+from .metrics import Evaluator, ari
 
 
 __all__ = [
@@ -9,5 +9,6 @@ __all__ = [
     'spatial_broadcast', 
     'build_grid', 
     'hungarian_huber_loss', 
-    'Evaluator'
+    'Evaluator',
+    'ari'
     ]
