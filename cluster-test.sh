cc8cd635431d2ac54248d30d43f63b2e21388517
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-30 01:22:52 +0300

    prepared clevr-tex test

--- cluster-test.sh
+++ cluster-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_sp_train
+#SBATCH --job-name=quantised_sa_od_train
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
@@ -28,7 +28,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --device 'gpu' --max_epochs 442 --batch_size 128 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 0 --nums 8 8 8 8 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --device 'gpu' --max_epochs 442 --batch_size 128 --train_path "/home/quantised_sa/datasets/clevr" --seed 0 --nums 8 8 8 8 --num_workers 4;
       free -m;
 " > output.txt;
 
