e32fa156d83ee62ac3a6cd4fd87147e703e8dd4b
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-01 20:02:27 +0300

    prepared clevr-tex test

--- utils/metrics.py
+++ utils/metrics.py
@@ -28,6 +28,33 @@ class RunningMean:
 
 
 def adjusted_rand_index(true_mask, pred_mask):
+    """Computes the adjusted Rand index (ARI), a clustering similarity score.
+    This implementation ignores points with no cluster label in `true_mask` (i.e.
+    those points for which `true_mask` is a zero vector). In the context of
+    segmentation, that means this function can ignore points in an image
+    corresponding to the background (i.e. not to an object).
+    Args:
+      true_mask: `Tensor` of shape [batch_size, n_points, n_true_groups].
+        The true cluster assignment encoded as one-hot.
+      pred_mask: `Tensor` of shape [batch_size, n_points, n_pred_groups].
+        The predicted cluster assignment encoded as categorical probabilities.
+        This function works on the argmax over axis 2.
+      name: str. Name of this operation (defaults to "ari_score").
+    Returns:
+      ARI scores as a Torch `Tensor` of shape [batch_size].
+    Raises:
+      ValueError: if n_points <= n_true_groups and n_points <= n_pred_groups.
+        We've chosen not to handle the special cases that can occur when you have
+        one cluster per datapoint (which would be unusual).
+    References:
+      Lawrence Hubert, Phipps Arabie. 1985. "Comparing partitions"
+        https://link.springer.com/article/10.1007/BF01908075
+      Wikipedia
+        https://en.wikipedia.org/wiki/Rand_index
+      Scikit Learn
+        http://scikit-learn.org/stable/modules/generated/\
+        sklearn.metrics.adjusted_rand_score.html
+    """
     _, n_points, n_true_groups = true_mask.shape
     n_pred_groups = pred_mask.shape[-1]
     assert not (n_points <= n_true_groups and n_points <= n_pred_groups), (
