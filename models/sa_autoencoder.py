ce25ae053607a1d3eaf468273c1e66e6ed315cbe
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-02 16:36:01 +0300

    prepared clevr-tex test

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -141,17 +141,18 @@ class SlotAttentionAE(pl.LightningModule):
             print("ATTENTION! MASKS (true/pred): ", true_masks.shape, pred_masks.shape, file=sys.stderr, flush=True)
             print("TRUE: ", true_masks[true_masks > 0], file=sys.stderr, flush=True)
             print("PRED: ", pred_masks, file=sys.stderr, flush=True)
-            self.log('ARI', adjusted_rand_index(pred_masks, true_masks))
+
             self.trainer.logger.experiment.log({
                 'images': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(imgs, -1, 1)],
                 'reconstructions': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(result, -1, 1)],
-                'true_masks': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(true_masks, -1, 1)],
+                'true_masks': [wandb.Image((x+100) / 2 + 0.5) for x in torch.clamp(true_masks, -1, 1)],
                 'pred_masks': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(pred_masks, -1, 1)]
             })
             self.trainer.logger.experiment.log({
                 f'{i} slot': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(recons[:, i], -1, 1)]
                 for i in range(self.num_slots)
             })
+            self.log('ARI', adjusted_rand_index(pred_masks.reshape(pred_masks[:2], -1), true_masks.reshape(true_masks[:2], -1)))
         return loss
 
     def validation_epoch_end(self, outputdata):
