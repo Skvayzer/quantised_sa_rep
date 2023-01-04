4a57e931883276e9a972edd4361181c8d07520af
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-04 04:53:54 +0300

    prepared tetrominoes

--- cluster-test.sh
+++ cluster-test.sh
@@ -29,7 +29,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task 'clevr-tex_from_checkpoint' --from_checkpoint './sa_autoencoder_end_to_end/430_2.0_sa_od_pretrained.ckpt' --device 'gpu' --max_epochs 442 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 0 --nums 8 8 8 8 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task 'clevr-tex_from_checkpoint' --from_checkpoint './sa_autoencoder_end_to_end/430_2.0_sa_od_pretrained.ckpt' --device 'gpu' --max_epochs 442 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 0 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
