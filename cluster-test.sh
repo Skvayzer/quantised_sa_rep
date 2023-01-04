f02347b3b1940057e49d5924149bef7412ba5882
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-04 04:57:53 +0300

    prepared tetrominoes

--- cluster-test.sh
+++ cluster-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_od_clevr_tex_end_to_end
+#SBATCH --job-name=quantised_sa_od_clevr_tex_end_to_end_8888
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
@@ -29,7 +29,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task 'clevr-tex_end_to_end' --from_checkpoint './sa_autoencoder_end_to_end/90_2.0_sa_od_pretrained.ckpt' --device 'gpu' --max_epochs 442 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 0 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task 'clevr-tex_end_to_end' --device 'gpu' --max_epochs 442 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 0 --nums 8 8 8 8 --num_workers 4;
       free -m;
 ";
 
--- training_od.py
+++ training_od.py
@@ -166,9 +166,9 @@ callbacks = [
     lr_monitor,
 ]
 
-# if args.pretrained:
-state_dict = torch.load(args.sa_state_dict)
-autoencoder.load_state_dict(state_dict=state_dict, strict=False)
+if args.pretrained:
+    state_dict = torch.load(args.sa_state_dict)
+    autoencoder.load_state_dict(state_dict=state_dict, strict=False)
 
 # ------------------------------------------------------------
 # Trainer
