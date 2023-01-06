f9a04379a7fc588704ef15035c44ffb7d066cc35
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-06 03:55:50 +0300

    prepared clevr-mirror

--- clevr-mirror.sh
+++ clevr-mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_sp_clevr_mirror_end_to_end
+#SBATCH --job-name=quantised_sa_od_clevr_mirror_end_to_end
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
@@ -29,7 +29,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_sp.py --dataset 'clevr-mirror' --task 'clevr_mirror_end_to_end' --device 'gpu' --max_epochs 1000 --batch_size 32 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 0 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-mirror' --task 'clevr_mirror_end_to_end' --device 'gpu' --max_epochs 1000 --batch_size 32 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 0 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
