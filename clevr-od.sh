5166b6f97414032b4bba5610a4ee7ed2f1817e87
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-14 21:15:15 +0300

    prepared clevr-tex

--- clevr-od.sh
+++ clevr-od.sh
@@ -29,7 +29,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'od_enc_to_end' --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr/od_enc_to_end/60_2.0_sa_od_pretrained.ckpt' --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --seed 0 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'od_enc_to_end' --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --seed 0 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
