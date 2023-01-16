c13a2c4078f4934eba630984ed727f2e9fe8a599
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-17 01:45:30 +0300

    prepared clevr-tex

--- clevr-tex-end-to-end.sh
+++ clevr-tex-end-to-end.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_od_clevr_tex_end_to_end
+#SBATCH --job-name=sa_od_clevr_tex_end_to_end
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=4
 ##SBATCH --time=0-0:05:00
-#SBATCH --partition=DGX-1v100
+#SBATCH --partition=titan_X
 ##SBATCH --gres=gpu:1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=16GB
@@ -29,7 +29,7 @@ singularity exec instance://ml_env /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task '60_4_3_end_to_end' --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 1 --nums 60 4 3 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-tex' --task '60_4_3_end_to_end' --no-quantization --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-tex" --seed 1 --nums 60 4 3 --num_workers 4;
       free -m;
 ";
 
