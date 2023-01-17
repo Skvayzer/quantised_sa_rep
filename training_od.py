929b4cdef1b9f00468c5e45184f4c5dd4408ab1d
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-17 03:10:28 +0300

    prepared celeba

--- training_od.py
+++ training_od.py
@@ -129,9 +129,9 @@ elif dataset == 'clevr-tex':
     collation = collate_fn
 elif dataset == 'celeba':
     transforms = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.ToPILImage(),
-        torchvision.transforms.Resize((128, 128)),
+        # torchvision.transforms.ToTensor(),
+        # torchvision.transforms.ToPILImage(),
+        # torchvision.transforms.Resize((128, 128)),
         # torchvision.transforms.CenterCrop((128, 128)),
         # torchvision.transforms.ToTensor()
     ])
