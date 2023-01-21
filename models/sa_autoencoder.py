637481bbc65d4f3f306675fb89da6fb16337bd60
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-21 21:04:14 +0300

    prepared clevr

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -156,6 +156,12 @@ class SlotAttentionAE(pl.LightningModule):
                 'images': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(imgs, -1, 1)],
                 'reconstructions': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(result, -1, 1)]
             })
+            if self.dataset in ['clevr-tex', 'clevr']:
+                self.trainer.logger.experiment.log({
+                    'images': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(imgs, -1, 1)],
+                    'reconstructions': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(result, -1, 1)],
+                    'true_mask': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(true_masks, -1, 1)]
+                })
             self.trainer.logger.experiment.log({
                 f'{i} slot': [wandb.Image(x / 2 + 0.5) for x in torch.clamp(recons[:, i], -1, 1)]
                 for i in range(self.num_slots)
