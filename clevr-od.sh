126a27e010f7092aa3b4fb590e854182df97b20c
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-22 19:18:42 +0300

    prepared clevr

--- clevr-od.sh
+++ clevr-od.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_od_clevr_beta0_end_to_end_masked
+#SBATCH --job-name=quantised_sa_od_clevr_beta1_end_to_end_masked
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
@@ -17,9 +17,9 @@
 singularity instance start \
                      --nv  \
                      --bind /home/AI/yudin.da/smirnov_cv/quantised_sa:/home/quantised_sa \
-                     ml_env.sif ml_env3
+                     ml_env.sif ml_env4
 
-singularity exec instance://ml_env3 /bin/bash -c "
+singularity exec instance://ml_env4 /bin/bash -c "
       source /miniconda/etc/profile.d/conda.sh;
       conda activate ml_env;
       export WANDB_API_KEY=c84312b58e94070d15277f8a5d58bb72e57be7fd;
@@ -29,8 +29,8 @@ singularity exec instance://ml_env3 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'beta=0' --beta 0 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 0 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'beta=1' --beta 1 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 0 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
-singularity instance stop ml_env3
+singularity instance stop ml_env4
