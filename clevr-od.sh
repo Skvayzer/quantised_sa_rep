f20ea8f37fa60e1a77b2a77de9e8e8d5684785f4
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-23 09:04:06 +0300

    prepared clevr

--- clevr-od.sh
+++ clevr-od.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_od_clevr_beta1_end_to_end_masked
+#SBATCH --job-name=quantised_sa_od_clevr_beta2_end_to_end_masked
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
@@ -29,7 +29,7 @@ singularity exec instance://ml_env4 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'beta=1' --beta 1 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 0 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --task 'beta=2' --beta 2 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 1 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
