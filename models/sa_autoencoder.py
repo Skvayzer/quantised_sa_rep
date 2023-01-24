ff8b2db8c19cb5f2f7045a48626f312055e5a3a4
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-24 21:14:33 +0300

    prepared tetraminoes

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -52,7 +52,8 @@ class SlotAttentionAE(pl.LightningModule):
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, hidden_size, kernel_size=5, padding=(2, 2)), nn.ReLU(),
             *[nn.Sequential(nn.Conv2d(hidden_size, hidden_size, kernel_size=5, padding=(2, 2)), nn.ReLU()) for _ in
-              range(3)]
+              range(2)],
+            nn.Conv2d(in_channels, 64, kernel_size=5, padding=(2, 2)), nn.ReLU(),
         )
         self.decoder_initial_size = (8, 8)
 
