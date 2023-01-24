c811330dbd796830bc052a6053b3bae468f17e9e
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-24 21:08:41 +0300

    prepared tetraminoes

--- modules/pos_embeds.py
+++ modules/pos_embeds.py
@@ -1,3 +1,5 @@
+import sys
+
 import torch
 from torch import nn
 
@@ -12,4 +14,8 @@ class PosEmbeds(nn.Module):
         
     def forward(self, inputs):
         pos_emb = self.linear(self.grid).moveaxis(3, 1)
+        print("\n\nATTENTION! inputs : ", inputs.shape, file=sys.stderr, flush=True)
+        print("\n\nATTENTION! pos_emb : ", pos_emb.shape, file=sys.stderr, flush=True)
+
+
         return inputs + pos_emb, pos_emb.expand(inputs.shape[0], -1, -1, -1)
