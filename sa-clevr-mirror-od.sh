828a64ac638d5b730d80b30cd59d683f93a80f9d
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-02-02 22:22:54 +0300

    prepared clevr-mirror

--- sa-clevr-mirror-od.sh
+++ sa-clevr-mirror-od.sh
@@ -29,7 +29,7 @@ singularity exec instance://ml_env9 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-mirror' --task 'slot attention' --no-quantization --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr-mirror/sa_end_to_end/10_2.0_sa_od_pretrained.ckpt' --beta 2 --device 'gpu' --max_epochs 2000 --batch_size 16 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 29 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-mirror' --task 'slot attention' --no-quantization --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr-mirror/sa_end_to_end/10_2.0_sa_od_pretrained.ckpt' --beta 2 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 29 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
