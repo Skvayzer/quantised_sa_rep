4a3984a8201e1b7c788093f22bc64e884371c8d5
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-02-05 12:48:28 +0300

    prepared clevr

--- clevr-od-from-checkpoint.sh
+++ clevr-od-from-checkpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=quantised_sa_od_clevr_ari_calc_end_to_end_checkpoint1
+#SBATCH --job-name=quantised_sa_od_clevr_end_to_end_from_sa
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=4
 ##SBATCH --time=0-0:05:00
-#SBATCH --partition=titan_X
+#SBATCH --partition=DGX-1v100
 ##SBATCH --gres=gpu:1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=16GB
@@ -17,9 +17,9 @@
 singularity instance start \
                      --nv  \
                      --bind /home/AI/yudin.da/smirnov_cv/quantised_sa:/home/quantised_sa \
-                     ml_env.sif ml_env7
+                     ml_env.sif ml_env1
 
-singularity exec instance://ml_env7 /bin/bash -c "
+singularity exec instance://ml_env1 /bin/bash -c "
       source /miniconda/etc/profile.d/conda.sh;
       conda activate ml_env;
       export WANDB_API_KEY=c84312b58e94070d15277f8a5d58bb72e57be7fd;
@@ -29,8 +29,8 @@ singularity exec instance://ml_env7 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --pretrained --sa_state_dict './quantised_sa_rep/clevr7_od' --task 'clevr from SA 15:51 03.02.2023' --beta 1 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 17 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --pretrained --sa_state_dict './quantised_sa_rep/clevr7_od' --task 'clevr from SA 12:44 05.02.2023' --beta 2 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr" --val_path "/home/quantised_sa/datasets/clevr_with_masks/clevr_with_masks" --seed 31 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
-singularity instance stop ml_env7
+singularity instance stop ml_env1
