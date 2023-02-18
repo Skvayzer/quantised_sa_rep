9403fbd39559876ee8bc7fa7fbc8919b958fe458
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-02-18 03:45:37 +0300

    fixes

--- clevr-mirror-od-from-checkpoint.sh
+++ clevr-mirror-od-from-checkpoint.sh
@@ -29,7 +29,7 @@ singularity exec instance://ml_env11 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-mirror' --task 'VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)' --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr-mirror/clevr-mirror/VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)/110_0.015625_VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)_clevr-mirror_od_pretrained.ckpt' --beta 0.015625 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 19 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr-mirror' --task 'VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)' --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr-mirror/VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)/110_0.015625_VQ-SA (SA based) clevr mirror 16.02.2023 (ckpt)_clevr-mirror_od_pretrained.ckpt' --beta 0.015625 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/clevr-mirror" --seed 19 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
