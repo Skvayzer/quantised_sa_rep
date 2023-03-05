3e30e1b1cc78ef737d3796e21408db584bb9fed8
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-05 17:20:46 +0300

    fixes

--- clevr-od-from-checkpoint.sh
+++ clevr-od-from-checkpoint.sh
@@ -29,7 +29,7 @@ singularity exec instance://ml_env7 /bin/bash -c "
       nvidia-smi;
       free -m;
       cd /home/quantised_sa;
-      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --from_checkpoint '/home/quantised_sa/sa_autoencoder_end_to_end/clevr/clevr from SA 24.02.2023/545_0.015625_clevr from SA 24.02.2023_clevr_od_pretrained.ckpt' --task 'clevr VQ-SA from SA 05.03.2023 (ckpt)' --beta 0.015625 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/sa_clevr/clevr_with_masks_6" --seed 31 --nums 8 3 2 2 --num_workers 4;
+      python3 -u quantised_sa_rep/training_od.py --dataset 'clevr' --pretrained --sa_state_dict '/home/quantised_sa/sa_autoencoder_end_to_end/clevr/clevr from SA 24.02.2023/545_0.015625_clevr from SA 24.02.2023_clevr_od_pretrained.ckpt' --task 'clevr VQ-SA from SA 05.03.2023 (ckpt)' --beta 0.015625 --device 'gpu' --max_epochs 2000 --batch_size 64 --train_path "/home/quantised_sa/datasets/sa_clevr/clevr_with_masks_6" --seed 31 --nums 8 3 2 2 --num_workers 4;
       free -m;
 ";
 
